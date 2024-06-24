83ee218ef32db59f73218ca92573a3bef7ceaf2e
Todd J Pieper <87354616+Taddioz@users.noreply.github.com>
2024-06-23 22:56:28 -0700

    Update generalVariables.py
    adding my name header to this file

--- SecretHitlerBot/generalVariables.py
+++ SecretHitlerBot/generalVariables.py
@@ -1,3 +1,12 @@
+"""
+Todd Pieper
+Created on Mon-Jul-17-2024
+Last edited: Sun-Jul-23-2024
+Implemented using discord.py API - https://discordpy.readthedocs.io/en/latest/index.html
+All work done here is my own, I do not give permission for one to copy and use it.
+Uploading all work to my GitHub ---
+"""
+
 # Variables used by both asyncNonCommands and generalFunctions
 playerList = []
 liberalList = []
@@ -9,11 +18,3 @@ originalPlayerCount = 0
 enactedFascistPolices = 0
 enactedLiberalPolices = 0
 vetoPowerUnlocked = False
-
-
-
-
-
-
-
-
